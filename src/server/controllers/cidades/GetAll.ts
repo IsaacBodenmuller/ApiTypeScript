e291b61599ac4bdaa92f7ca869962ab73e913b51
IsaacBodenmuller <isaac23gb@gmail.com>
2026-01-16 17:52:31 -0300

    feat: some adjusts

---
+++ src/server/controllers/cidades/GetAll.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import * as yup from "yup";
+import { validation } from "../../shared/middleware";
+import { StatusCodes } from "http-status-codes";
+
+export interface IQueryProps {
+  page?: number | undefined;
+  limit?: number | undefined;
+  filter?: string | undefined;
+}
+
+export const getAllValidation = validation((getSchema) => ({
+  query: getSchema<IQueryProps>(
+    yup.object().shape({
+      page: yup.number().optional().moreThan(0),
+      limit: yup.number().optional().moreThan(0),
+      filter: yup.string().optional(),
+    })
+  ),
+}));
+
+export const getAll = async (
+  req: Request<{}, {}, {}, IQueryProps>,
+  res: Response
+) => {
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .send("Não implementado!");
+};
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,6 +8,11 @@ router.get("/", (_, res) => {
   return res.send("Opa dev");
 });
 
+router.get(
+  "/cidades",
+  CidadesController.getAllValidation,
+  CidadesController.getAll
+);
 router.post(
   "/cidades",
   CidadesController.createValidation,
